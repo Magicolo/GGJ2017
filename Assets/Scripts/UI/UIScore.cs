d463ff0dd77096f5976046ba687a91b0ba7d53af
Magicolo <straty91@hotmail.com>
2017-01-21 03:04:20 -0500

    some nice UI :D

--- Assets/Scripts/UI/UIScore.cs
+++ Assets/Scripts/UI/UIScore.cs
@@ -11,6 +11,11 @@ public class UIScore : MonoBehaviour
 
 	void Update()
 	{
-		Text.text = string.Format("{0}% Light Speed", LevelManager.Instance.LightSpeedRatio * 100f);
+		float ratio = LevelManager.Instance.LightSpeedRatio;
+		Text.text = string.Format("{0}% Light Speed", ratio * 100f);
+
+		float danger = Mathf.Pow(ratio, 8f);
+		float random = UnityEngine.Random.Range(-danger, danger);
+		Text.color = new Color(Mathf.Pow(ratio, 2f) + random, 1f - danger + random, 1f - danger + random, 1f);
 	}
 }
