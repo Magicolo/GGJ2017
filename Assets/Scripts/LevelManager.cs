826e61d53b4aa376ddcb301469cc7182347b1745
Magicolo <straty91@hotmail.com>
2017-01-21 02:10:20 -0500

    score + crash on too much speed

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -13,8 +14,19 @@ public class LevelManager : MonoBehaviour
 	public Vector2 PlayerBounds = new Vector2(0f, 0.75f);
 	public float Speed = 0.5f;
 
+	public float LightSpeedRatio
+	{
+		get { return 1f - 1f / (Speed * 4f); }
+	}
+
 	void Awake()
 	{
 		Instance = this;
+	}
+
+	void Update()
+	{
+		if (LightSpeedRatio >= 1f)
+			SceneManager.LoadScene("Crash");
 	}
 }
---
+++ Assets/Scripts/UI/UIScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIScore : MonoBehaviour
+{
+	public Text Text;
+
+	void Update()
+	{
+		Text.text = string.Format("{0}% Light Speed", LevelManager.Instance.LightSpeedRatio * 100f);
+	}
+}
