658a9aaa30a7758d661581cb1b80a23fa1ad22f7
Richard <rail.rich@gmail.com>
2017-01-21 18:48:25 -0500

    as

--- Assets/Scripts/zTest.cs
+++ Assets/Scripts/zTest.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class zTest : MonoBehaviour
 {
+	//int frame
 
+	private void Update()
+	{
+		string thing = "";
+		for (int i = 1; i <= 8; i++)
+		{
+			thing += Input.GetAxis("Horizontal" + i) + " - ";
+		}
+		Debug.Log(thing);
+
+	}
 }
