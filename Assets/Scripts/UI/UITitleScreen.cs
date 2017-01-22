a5d42b85cec346837221c9a91b5da05284d2ca56
Magicolo <straty91@hotmail.com>
2017-01-22 13:08:58 -0500

    intro scene and credits

---
+++ Assets/Scripts/UI/UITitleScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class UITitleScreen : MonoBehaviour
+{
+	public void OnStart()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void OnCredits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+}
